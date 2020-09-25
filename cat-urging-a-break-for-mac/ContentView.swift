b77d98bed7e45d94894d16bdd551148e30240f42
hanamizuki10 <mizuki.hana@imprich-create.site>
2020-09-25 14:53:53 +0900

    Initial Commit

---
+++ cat-urging-a-break-for-mac/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  cat-urging-a-break-for-mac
+//
+//  Created by hanamizuki on 2020/09/25.
+//  Copyright © 2020 hanamizuki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
